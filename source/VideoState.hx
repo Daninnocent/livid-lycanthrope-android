9719ec147ccb03415252c3f1812eb6a33bef7124
Daninnocent <58911390+Daninnocent@users.noreply.github.com>
2021-11-25 16:34:45 +0700

    Bruhhh why is this not workinggggg

--- source/VideoState.hx
+++ source/VideoState.hx
@@ -1,10 +1,9 @@
 package;
 
-import flixel.FlxCamera;
-import Achievements;
 import flixel.text.FlxText;
 import flixel.FlxState;
 import flixel.FlxG;
+import flixel.FlxSubState;
 
 import extension.webview.WebView;
 
@@ -18,26 +17,18 @@ class VideoState extends MusicBeatState
 
 	var text:FlxText;
 
-	private var camAchievement:FlxCamera;
-
-	//var firsttimeeaster:Bool = true;
-	//Solucao burra
-
 	public function new(source:String, toTrans:FlxState)
 	{
 		super();
 
-		camAchievement = new FlxCamera();
-		camAchievement.bgColor.alpha = 0;
-	
-		FlxG.cameras.add(camAchievement);
-
-		text = new FlxText(0, 0, 0, "Tap to skip the video", 48); //Eu aprendi a mexer  nas opçoes só pra isso, por favor me mata // What are you saying matheus lmao
+		text = new FlxText(0, 0, 0, "tap to continue", 48);
 		text.screenCenter();
 		text.alpha = 0;
 		add(text);
 
 		nextState = toTrans;
+
+		//FlxG.autoPause = false;
 
 		WebView.onClose=onClose;
 		WebView.onURLChanging=onURLChanging;
@@ -48,18 +39,22 @@ class VideoState extends MusicBeatState
 	public override function update(dt:Float) {
 		for (touch in FlxG.touches.list)
 			if (touch.justReleased)
-				onClose(); //hmmmm maybe
+				onClose();
 
 		super.update(dt);	
 	}
 
 	function onClose(){// not working
 		text.alpha = 0;
-		MusicBeatState.switchState(nextState);
+		//FlxG.autoPause = true;
+		trace('close!');
+		trace(nextState);
+		FlxG.switchState(nextState);
 	}
 
 	function onURLChanging(url:String) {
 		text.alpha = 1;
-		if (url == 'http://exitme(.*)') onClose(); // drity hack lol
+		if (url == 'http://exitme/') onClose(); // drity hack lol
+		trace("WebView is about to open: "+url);
 	}
 }
